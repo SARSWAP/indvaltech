bd80dbf587499822542527062560fe2a1f36ba29
Adarsh Singh Nishen <adarshnishen25@gmail.com>
2022-06-20 02:47:22 +0530

    views changed

--- hrms/settings.py
+++ hrms/settings.py
@@ -124,8 +124,8 @@ CSRF_TRUSTED_ORIGINS = [
 
 EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = os.environ['EMAIL_HOST']
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
-EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
+EMAIL_HOST_USER = 'mysterysoul459@gmail.com'
+EMAIL_HOST_PASSWORD = 'aiujtvctjvqmtqqw'
