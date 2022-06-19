b22638724598947e9a05d051edb49f32c9f53e79
Adarsh Singh Nishen <adarshnishen25@gmail.com>
2022-06-19 16:13:49 +0530

    SMTP added

--- hrms/settings.py
+++ hrms/settings.py
@@ -131,3 +131,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CSRF_TRUSTED_ORIGINS = [
     'https://indval2022.herokuapp.com'
 ]
+
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "mysterysoul459@gmail.com"
+EMAIL_HOST_PASSWORD = "aiujtvctjvqmtqqw"
